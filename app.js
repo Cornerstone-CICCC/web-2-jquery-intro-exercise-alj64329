d6f0a242e77bcfd6fd207f6218845298cb0797f7
Ayaka Miyazaki <kuromi.1031blc@gmail.com>
2025-08-05 19:32:43 -0700

    Done with assingment

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
 $(function() {
   // YOUR CODE HERE
+  // Ex. 1
+  $('#toggleButton').on('click', function(){
+    $('#myParagraph').toggle()
+  })
+
+  // Ex 2.
+  $('#colorButton').on('click', function(){
+    $('#colorDiv').css('background-color','yellow')
+  })
+
+  // Ex 3
+  $('#addClassButton').on('click', function(){
+    $('#classDiv').addClass('newClass')
+  })
+  $('#removeClassButton').on('click', function(){
+    $('#classDiv').removeClass('newClass')
+  })
+
+  // Ex. 4
+  $('#fadeInButton').on('click', function(){
+    $('#fadeDiv').fadeIn()
+  })
+  $('#fadeOutButton').on('click', function(){
+    $('#fadeDiv').fadeOut()
+  })
+
+  // Ex. 5
+  $('#slideUpButton').on('click', function(){
+    $('#slideDiv').slideUp()
+  })
+  $('#slideDownButton').on('click', function(){
+    $('#slideDiv').slideDown()
+  })
 })
